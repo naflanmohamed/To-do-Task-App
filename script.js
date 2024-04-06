515ff00e2baa490d6f77c6e265f13c77cebf8e1b
naflanmohamed <naflanmohammed611@gmail.com>
2024-04-06 07:08:39 +0500

    JS File

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
+
 document.getElementById('add').addEventListener('click', function() {
-    var taskValue = document.getElementById('task').value;
-    if (taskValue.trim() !== "")
-    {
-        var taskItem = document.createElement('div');
+    const taskTitle = document.getElementById('taskTitle').value;
+    const taskDescription = document.getElementById('taskDescription').value;
+    
+    if (taskTitle && taskDescription) {
+        const taskItem = document.createElement('div');
         taskItem.classList.add('task-item');
         
+        taskItem.innerHTML = `<p><strong>Title:</strong> ${taskTitle} <br> <strong>Description:</strong> ${taskDescription}</p>`;
         
-        var taskText = document.createElement('span');
-        taskText.textContent = taskValue;
-        taskItem.appendChild(taskText);
-        
-        var deleteBtn = document.createElement('i');
-        deleteBtn.classList.add('fa-solid', 'fa-trash', 'delete-btn');
+        const deleteBtn = document.createElement('i');
+        deleteBtn.classList.add('fa-solid', 'fa-trash-can', 'delete-btn');
         taskItem.appendChild(deleteBtn);
         
-        document.getElementById('taskList').appendChild(taskItem);
-        
-        document.getElementById('task').value = "";
-        
+        document.getElementById('tasks').appendChild(taskItem);
+
+        document.getElementById('taskTitle').value = '';
+        document.getElementById('taskDescription').value = '';
+
         deleteBtn.addEventListener('click', function() {
             taskItem.remove();
         });
+    } 
+    else {
+        alert('Please Fill in both the Title and Description');
     }
 });
